cb5b0fc4560fcf1ac18e357c5cc2b236e8cef1b1
Vistad16 <bohdan.datsiv@gmail.com>
2022-05-04 16:32:08 +0300

    format code

--- src/main/java/module2/Main.java
+++ src/main/java/module2/Main.java
@@ -1,8 +1,8 @@
 package module2;
 
 public class Main {
-    public static void main(String[] args) {
-        double test = new Calculate().calculateTotalCost("ABCDABA");// expected 13.25
-        System.out.println(test);
-    }
+  public static void main(String[] args) {
+    double test = new Calculate().calculateTotalCost("ABCDABA"); // expected 13.25
+    System.out.println(test);
+  }
 }
--- src/test/java/javaDeveloper/module2/CalculateTest.java
+++ src/test/java/javaDeveloper/module2/CalculateTest.java
@@ -11,64 +11,63 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculateTest {
 
-    @Test
-    public void testThatCalculateTotalCostForEmptyValues() {
-        double actual = new Calculate().calculateTotalCost("");
-        double expected = 0.0;
+  @Test
+  public void testThatCalculateTotalCostForEmptyValues() {
+    double actual = new Calculate().calculateTotalCost("");
+    double expected = 0.0;
 
-        Assertions.assertEquals(expected, actual);
-    }
+    Assertions.assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testThatCalculateTotalCostWorksOk() {
-        Map<String, Double> testCase = new HashMap<>();
-        testCase.put("A", 1.25);
-        testCase.put("B", 4.25);
-        testCase.put("C", 1.00);
-        testCase.put("D", 0.75);
+  @Test
+  public void testThatCalculateTotalCostWorksOk() {
+    Map<String, Double> testCase = new HashMap<>();
+    testCase.put("A", 1.25);
+    testCase.put("B", 4.25);
+    testCase.put("C", 1.00);
+    testCase.put("D", 0.75);
 
-        testCase.forEach((code, price) ->
-                Assertions.assertEquals(price, new Calculate().calculateTotalCost(code), code));
+    testCase.forEach(
+        (code, price) ->
+            Assertions.assertEquals(price, new Calculate().calculateTotalCost(code), code));
+  }
 
-    }
+  @Test
+  public void testThatCalculateTotalCostForDifferentValues() {
+    double actual = new Calculate().calculateTotalCost("ABCD");
+    double expected = 7.25;
 
-    @Test
-    public void testThatCalculateTotalCostForDifferentValues() {
-        double actual = new Calculate().calculateTotalCost("ABCD");
-        double expected = 7.25;
+    Assertions.assertEquals(expected, actual);
+  }
 
-        Assertions.assertEquals(expected, actual);
-    }
+  @Test
+  public void testThatCalculateTotalCostForDiscountA() {
+    double actual = new Calculate().calculateTotalCost("AAA");
+    double expected = 3.00;
 
-    @Test
-    public void testThatCalculateTotalCostForDiscountA() {
-        double actual = new Calculate().calculateTotalCost("AAA");
-        double expected = 3.00;
+    Assertions.assertEquals(expected, actual);
+  }
 
-        Assertions.assertEquals(expected, actual);
-    }
+  @Test
+  public void testThatCalculateTotalCostForDiscountC() {
+    double actual = new Calculate().calculateTotalCost("CCCCCC");
+    double expected = 5.00;
 
-    @Test
-    public void testThatCalculateTotalCostForDiscountC() {
-        double actual = new Calculate().calculateTotalCost("CCCCCC");
-        double expected = 5.00;
+    Assertions.assertEquals(expected, actual);
+  }
 
-        Assertions.assertEquals(expected, actual);
-    }
+  @Test
+  public void testThatCalculateTotalCostMaxCases() {
+    double actual = new Calculate().calculateTotalCost("CACBCACACDCAC");
+    double expected = 15.25;
 
-    @Test
-    public void testThatCalculateTotalCostMaxCases() {
-        double actual = new Calculate().calculateTotalCost("CACBCACACDCAC");
-        double expected = 15.25;
+    Assertions.assertEquals(expected, actual);
+  }
 
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testThatCalculateTotalCostThrowsException() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Calculate().calculateTotalCost("CACBCA CACDCAC");
-        });
-    }
-
+  @Test
+  public void testThatCalculateTotalCostThrowsException() {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new Calculate().calculateTotalCost("CACBCA CACDCAC"));
+  }
 }
