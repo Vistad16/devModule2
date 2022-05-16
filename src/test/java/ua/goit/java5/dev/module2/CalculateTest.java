f4e9764a0507668c2fd82c29267bd4ebed9ab279
Vistad16 <bohdan.datsiv@gmail.com>
2022-05-16 10:39:58 +0300

    refactoring tests

--- src/test/java/ua/goit/java5/dev/module2/CalculateTest.java
+++ src/test/java/ua/goit/java5/dev/module2/CalculateTest.java
@@ -3,44 +3,48 @@ package ua.goit.java5.dev.module2;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ua.goit.java5.dev.module2.service.HardcodedDataBase;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CalculateTest {
 
   @InjectMocks Calculate calculate;
 
-  // Very-very close
-  // the last step: in the testThatDataBaseWithIdA you mocked the call to dataBase object and then
-  // asserts the same
-  // we shouldn't test the mockito, we have to test the Calculate class
+  @Mock HardcodedDataBase dataBase;
 
-  // For example testThatCalculateTotalCostForDifferentValues
-  // you have to you have to Mock all calls to the dataBase during the calculateTotalCost method in
-  // the same test
+  @Test
+  public void testThatCalculateTotalCostWithMockitoForA() {
+    when(dataBase.getGoods('A')).thenReturn(new Goods(1.25, 3, 0.25));
+    double expected = 1.25;
+    double actual = calculate.calculateTotalCost("A");
 
-  // to summarise - we have to test Calculate class but in the test we have to mock DataBase usages
-  // inside the method
-
-  // it can be difficult. It's because you code structure. Code should be testable, maybe it's will
-  // be better refactoring it a little
+    assertEquals(expected, actual);
+  }
 
   @Test
   public void testThatCalculateTotalCostForEmptyValues() {
     double expected = 0.0;
     double actual = calculate.calculateTotalCost("");
 
-    assertEquals(expected, actual);
+    assertEquals(expected, actual, "EmptyValuesTest");
   }
 
   @Test
   public void testThatCalculateTotalCostWorksOk() {
+    when(dataBase.getGoods('A')).thenReturn(new Goods(1.25, 3, 0.25));
+    when(dataBase.getGoods('B')).thenReturn(new Goods(4.25, 0, 0.00));
+    when(dataBase.getGoods('C')).thenReturn(new Goods(1.00, 6, 0.1666666666666666666667));
+    when(dataBase.getGoods('D')).thenReturn(new Goods(0.75, 0, 0.00));
+
     Map<String, Double> testCase = new HashMap<>();
     testCase.put("A", 1.25);
     testCase.put("B", 4.25);
@@ -53,34 +57,44 @@ class CalculateTest {
 
   @Test
   public void testThatCalculateTotalCostForDifferentValues() {
+    when(dataBase.getGoods('A')).thenReturn(new Goods(1.25, 3, 0.25));
+    when(dataBase.getGoods('B')).thenReturn(new Goods(4.25, 0, 0.00));
+    when(dataBase.getGoods('C')).thenReturn(new Goods(1.00, 6, 0.1666666666666666666667));
+    when(dataBase.getGoods('D')).thenReturn(new Goods(0.75, 0, 0.00));
     double expected = 7.25;
     double actual = calculate.calculateTotalCost("ABCD");
 
-    assertEquals(expected, actual, "Invalid: ABCD");
+    assertEquals(expected, actual, "ABCD");
   }
 
   @Test
   public void testThatCalculateTotalCostForDiscountA() {
+    when(dataBase.getGoods('A')).thenReturn(new Goods(1.25, 3, 0.25));
     double expected = 3.00;
     double actual = calculate.calculateTotalCost("AAA");
 
-    assertEquals(expected, actual);
+    assertEquals(expected, actual, "AAA");
   }
 
   @Test
   public void testThatCalculateTotalCostForDiscountC() {
+    when(dataBase.getGoods('C')).thenReturn(new Goods(1.00, 6, 0.1666666666666666666667));
     double expected = 5.00;
     double actual = calculate.calculateTotalCost("CCCCCC");
 
-    assertEquals(expected, actual);
+    assertEquals(expected, actual, "CCCCCC");
   }
 
   @Test
   public void testThatCalculateTotalCostMaxCases() {
+    when(dataBase.getGoods('A')).thenReturn(new Goods(1.25, 3, 0.25));
+    when(dataBase.getGoods('B')).thenReturn(new Goods(4.25, 0, 0.00));
+    when(dataBase.getGoods('C')).thenReturn(new Goods(1.00, 6, 0.1666666666666666666667));
+    when(dataBase.getGoods('D')).thenReturn(new Goods(0.75, 0, 0.00));
     double expected = 15.25;
     double actual = calculate.calculateTotalCost("CACBCACACDCAC");
 
-    assertEquals(expected, actual);
+    assertEquals(expected, actual, "CACBCACACDCAC");
   }
 
   @Test
